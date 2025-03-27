fc75a27138ff2ccc78792e723776b7430382d2fd
James Husband <jchusband@proton.me>
2025-03-27 21:32:43 +0000

    feat: create engine lib

--- apps/kuchen/src/app/app.tsx
+++ apps/kuchen/src/app/app.tsx
@@ -3,5 +3,5 @@ export default function App() {
     <div>
       <h1>Welcome</h1>
     </div>
-  )
+  );
 }
---
+++ libs/engine/src/lib/engine.spec.ts
@@ -0,0 +1,7 @@
+import { engine } from './engine.js'
+
+describe('engine', () => {
+  it('should work', () => {
+    expect(engine()).toEqual('engine')
+  })
+})
