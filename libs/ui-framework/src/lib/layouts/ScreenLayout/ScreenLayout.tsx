acbf88642b5c9a333007ae30672e223aa244ebc4
James Husband <jchusband@proton.me>
2025-03-29 23:52:47 +0000

    feat: bg colour

--- libs/ui-framework/src/lib/layouts/ScreenLayout/ScreenLayout.tsx
+++ libs/ui-framework/src/lib/layouts/ScreenLayout/ScreenLayout.tsx
@@ -1,6 +1,6 @@
 export const ScreenLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex items-center justify-center min-h-screen" data-testid="screen-layout">
+    <div className="flex items-center justify-center min-h-screen bg-gray-900" data-testid="screen-layout">
       <div className="w-[800px] h-[600px]" data-testid="game-container">
         {children}
       </div>
