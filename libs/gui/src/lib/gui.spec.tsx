af993bac97f8b6241ba1b3b699a46a09176b740f
James Husband <jchusband@proton.me>
2025-04-03 18:37:36 +0100

    feat: add gui lib

---
+++ libs/gui/src/lib/gui.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+
+import Gui from './gui';
+
+describe('Gui', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<Gui />);
+    expect(baseElement).toBeTruthy();
+  });
+});
---
+++ libs/gui/src/lib/gui.tsx
@@ -0,0 +1,9 @@
+export function Gui() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Gui!</h1>
+    </div>
+  );
+}
+
+export default Gui;
