4c7a3a0478dcc37909bb84f9fc2724c7969ad09a
James Husband <jchusband@proton.me>
2025-06-22 15:43:01 +0100

    basic jump event

--- libs/engine/src/lib/scene/lib/scenes/scene.game.ts
+++ libs/engine/src/lib/scene/lib/scenes/scene.game.ts
@@ -1,6 +1,23 @@
 import { createScene } from '../create-scene';
+import { eventBus } from '../../../events';
 
 export const createGameScene = () =>
   createScene('Game', (scene) => {
-    scene.add.rectangle(400, 300, 100, 100, 0xff0000);
+    const rect = scene.add.rectangle(400, 300, 100, 100, 0xff0000);
+    let isJumping = false;
+
+    eventBus.on('jump', () => {
+      if (isJumping) return;
+      isJumping = true;
+      scene.tweens.add({
+        targets: rect,
+        y: rect.y - 100,
+        duration: 200,
+        yoyo: true,
+        ease: 'Power2',
+        onComplete: () => {
+          isJumping = false;
+        },
+      });
+    });
   });
